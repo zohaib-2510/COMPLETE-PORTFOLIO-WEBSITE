1e36078a3cfdb3a904007620a6e540e2bd000ac7
zohaib-2510 <140811807+zohaib-2510@users.noreply.github.com>
2023-09-07 13:16:16 -0700

    2 PAGE FULL COMPLETE

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function isDs(){
-    var a = document .getElementById('cvcv').innerHTML='FACERE TOTAM! NULLA NOBIS ILLO RERUM SED EX MAIORES FACILIS MINUS AT? EXERCITATIONEM MAGNI QUOD NEQUE NUMQUAM REICIENDIS ATQUE POSSIMUS TEMPORA IMPEDIT, QUAM NEMO A'
-    document.getElementById('nbnbnb').style.display='none'
+function sbKa(){
+    var a = document .getElementById('orp').innerHTML='FACERE TOTAM! NULLA NOBIS ILLO RERUM SED EX MAIORES FACILIS MINUS AT? EXERCITATIONEM MAGNI QUOD NEQUE NUMQUAM REICIENDIS ATQUE POSSIMUS TEMPORA IMPEDIT, QUAM NEMO A'
+    document.getElementById('nbnb').style.display='none'
 }
